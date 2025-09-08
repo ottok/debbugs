f3c899dec08b2e5b530c903ca51470be2fbf6b17
Otto Kekäläinen <otto@debian.org>
2025-09-08 05:35:04 +0000

    Make Search::Estraier dependency optional to fix autopkgtest failures
    Load Search::Estraier conditionally using eval to prevent compilation
    failures when the module is not installed. Provide dummy subroutines
    for add_bug_log, add_bug_message, and remove_old_messages when the
    module is unavailable.
    
    This allows Debbugs::Estraier to be loaded without error during syntax
    checks, ensuring autopkgtest passes even when Search::Estraier is not
    present.
    
    Fixes autopkgtest error:
    
        Can't locate Search::Estraier.pm in @INC

--- lib/Debbugs/Estraier.pm
+++ lib/Debbugs/Estraier.pm
@@ -27,19 +27,35 @@ None known.
 
 use warnings;
 use strict;
-use vars qw($VERSION $DEBUG %EXPORT_TAGS @EXPORT_OK @EXPORT);
+our ($VERSION, $DEBUG);
+BEGIN {
+    ($VERSION) = q$Revision: 1.3 $ =~ /^Revision:\s+([^\s+])/;
+    $DEBUG = 0 unless defined $DEBUG;
+}
+
+eval "use Search::Estraier";
+if ($@) {
+    require Exporter;
+    our @ISA = qw(Exporter);
+    our (@EXPORT_OK, %EXPORT_TAGS);
+    @EXPORT_OK = qw(add_bug_log add_bug_message);
+    %EXPORT_TAGS = (add => \@EXPORT_OK, all => \@EXPORT_OK);
+    sub add_bug_log { return; }
+    sub add_bug_message { return; }
+    sub remove_old_messages { return; }
+    return 1;
+}
+
+# If we are here, Search::Estraier is loaded.
+use vars qw(%EXPORT_TAGS @EXPORT_OK @EXPORT);
 use Exporter qw(import);
 use Debbugs::Log;
-use Search::Estraier;
 use Debbugs::Common qw(getbuglocation getbugcomponent make_list);
 use Debbugs::Status qw(readbug);
 use Debbugs::MIME qw(parse);
 use Encode qw(encode_utf8);
 
 BEGIN{
-     ($VERSION) = q$Revision: 1.3 $ =~ /^Revision:\s+([^\s+])/;
-     $DEBUG = 0 unless defined $DEBUG;
-
      @EXPORT = ();
      %EXPORT_TAGS = (add    => [qw(add_bug_log add_bug_message)],
 		    );
